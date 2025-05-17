728a3e1509b0462fd55f2b064dd8b782f5e43c3f
siva1213 <sakunthalasivam@gmail.com>
2025-05-17 18:27:30 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.saucedemo.com',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'on',
     launchOptions: {
