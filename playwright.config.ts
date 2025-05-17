e7641f01fd47b8b9b35deeed0fbb98d9eda6be41
siva1213 <sakunthalasivam@gmail.com>
2025-05-17 21:12:15 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://www.saucedemo.com'
+    baseURL: 'https://www.saucedemo.com',
     headless: true,
     screenshot: 'only-on-failure',
     video: 'on',
